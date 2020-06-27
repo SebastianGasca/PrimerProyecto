8f951e2af629e5f74dd176244b6c96f8204163e1
SebastianGasca <sebagasca@gmail.com>
2020-06-27 19:24:36 -0400

    miprimercommit

---
+++ main.js
@@ -0,0 +1,14 @@
+const name = "nombre";
+var hola = 1;
+let perro = {
+    años:"21",
+    altura: "21cm"
+}; 
+
+console.log(perro);
+
+let saludos = "Buena Socie"; 
+
+const saludo = () => {
+    alert(`saludo ${saludos}`)
+}
